0b70dd0b22fa35976e48cb97a151f916a9480aa6
thejjwang <jonathanjacobwang@gmail.com>
2023-06-21 17:53:23 -0700

    works properly

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let btn = document.getElementById('btn');
 let jokeContainer = document.getElementById('joke-container');
 let joke = document.getElementById('joke');
-
+let btn2 = document.getElementById('btn2');
+let jokeContainer2 = document.getElementById('joke-container2');
+let joke2 = document.getElementById('joke2');
 
 btn.addEventListener('click', fetchApi);
 function fetchApi() {
@@ -18,4 +20,20 @@ function fetchApi() {
                 jokeContainer.style.display = 'block';
             })
            .catch(err => console.log(err));
+    }
+btn2.addEventListener('click', fetchApi2);
+function fetchApi2() {
+    fetch('https://api.chucknorris.io/jokes/random', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+            .then(response => response.json())
+            .then(data => {
+                joke2.innerText = data.value;
+                jokeContainer2.style.display = 'block';
+            })
+            .catch(err => console.log(err));
     }
