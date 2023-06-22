fbdbad3857050af5f36e9b0744260b38c841e358
thejjwang <jonathanjacobwang@gmail.com>
2023-06-22 15:33:34 -0700

    refactored

--- index.js
+++ index.js
@@ -1,39 +1,20 @@
 let btn = document.getElementById('btn');
-let jokeContainer = document.getElementById('joke-container');
 let joke = document.getElementById('joke');
-let btn2 = document.getElementById('btn2');
-let jokeContainer2 = document.getElementById('joke-container2');
 let joke2 = document.getElementById('joke2');
+let joke3 = document.getElementById('joke3');
+let joke4 = document.getElementById('joke4');
+let joke5 = document.getElementById('joke5');
+let joke6 = document.getElementById('joke6');
 
-btn.addEventListener('click', fetchApi);
-function fetchApi() {
-    fetch('https://icanhazdadjoke.com/', {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
+
+btn.addEventListener('click', myFunction);
+function myFunction(){
+    fetch('https://api.chucknorris.io/jokes/random?category=science', {
+        method: "GET"
+})
+    .then(response => response.json())
+    .then(data => {
+            joke.innerText = data.value;
+            console.log(data)
         })
-           .then(response => response.json())
-           .then(data => {
-                joke.innerText = data.joke;
-                jokeContainer.style.display = 'block';
-            })
-           .catch(err => console.log(err));
-    }
-btn2.addEventListener('click', fetchApi2);
-function fetchApi2() {
-    fetch('https://api.chucknorris.io/jokes/random', {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
         }
-    })
-            .then(response => response.json())
-            .then(data => {
-                joke2.innerText = data.value;
-                jokeContainer2.style.display = 'block';
-            })
-            .catch(err => console.log(err));
-    }
